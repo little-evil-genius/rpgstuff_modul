cac00ac48a82566f5933c6b140c3c6cff485d284
Lara <75126217+little-evil-genius@users.noreply.github.com>
2024-09-13 16:53:02 +0200

    Update module_meta.php

--- admin/modules/rpgstuff/module_meta.php
+++ admin/modules/rpgstuff/module_meta.php
@@ -1,10 +1,7 @@
 <?php
 /**
- * MyBB 1.8
  * Copyright 2014 MyBB Group, All Rights Reserved
- *
- * Website: http://www.mybb.com
- * License: http://www.mybb.com/about/license
+ * @author risuena & little.evil.genius
  *
  */
 
@@ -24,7 +21,7 @@ function rpgstuff_meta()
 	$sub_menu = array();
 	$sub_menu = $plugins->run_hooks("admin_rpgstuff_menu", $sub_menu);
 
-	$page->add_menu_item("RPG Erweiterungen", "rpgstuff", "index.php?module=rpgstuff", 60, $sub_menu);
+	$page->add_menu_item($lang->rpg_stuff, "rpgstuff", "index.php?module=rpgstuff", 60, $sub_menu);
 
 	return true;
 }
@@ -42,24 +39,22 @@ function rpgstuff_action_handler($action)
 
 	$actions = array(
 		'stylesheet_updates' => array('active' => 'stylesheet_updates', 'file' => 'stylesheet_updates.php'),
-		'template_updates' => array('active' => 'template_updates', 'file' => 'template_updates.php'),
 		'plugin_updates' => array('active' => 'plugin_updates', 'file' => 'plugin_updates.php')
     );
 	$actions = $plugins->run_hooks("admin_rpgstuff_action_handler", $actions);
 
 	$sub_menu = array();
-	$sub_menu['10'] = array("id" => "stylesheet_updates", "title" => "Stylesheets überprüfen", "link" => "index.php?module=rpgstuff-stylesheet_updates");
-	$sub_menu['20'] = array("id" => "template_updates", "title" => "Templates überprüfen", "link" => "index.php?module=rpgstuff-template_updates");
-	$sub_menu['30'] = array("id" => "plugin_updates", "title" => "Plugins aktualisieren", "link" => "index.php?module=rpgstuff-plugin_updates");
+	$sub_menu['10'] = array("id" => "stylesheet_updates", "title" => $lang->stylesheet_updates, "link" => "index.php?module=rpgstuff-stylesheet_updates");
+	$sub_menu['20'] = array("id" => "plugin_updates", "title" => $lang->plugin_updates, "link" => "index.php?module=rpgstuff-plugin_updates");
 
 	$sub_menu = $plugins->run_hooks("admin_rpgstuff_menu_updates", $sub_menu);
 
 	if(!isset($actions[$action]))
 	{
-		$page->active_action = $action = "plugin_updates";
+		$page->active_action = $action = "stylesheet_updates";
 	}
 
-	$sidebar = new SidebarItem("Updates");
+	$sidebar = new SidebarItem($lang->sidebar);
 	$sidebar->add_menu_items($sub_menu, $actions[$action]['active']);
 
 	$page->sidebar .= $sidebar->get_markup();
@@ -71,7 +66,7 @@ function rpgstuff_action_handler($action)
 	}
 	else
 	{
-		return "plugin_updates.php";
+		return "stylesheet_updates.php";
 	}
 }
 
@@ -83,12 +78,11 @@ function rpgstuff_admin_permissions()
 	global $lang, $plugins;
 
 	$admin_permissions = array(
-		"stylesheet_updates" => "Kann Stylesheets von Plugins updaten?",
-		"template_updates" => "Kann Templates von Plugins updaten?",
-		"plugin_updates" => "Kann Plugins updaten?"
+		"stylesheet_updates" => $lang->can_updates_stylesheet,
+		"plugin_updates" => $lang->can_updates_plugin
     );
 
 	$admin_permissions = $plugins->run_hooks("admin_rpgstuff_permissions", $admin_permissions);
 
-	return array("name" => "RPG Erweiterungen", "permissions" => $admin_permissions, "disporder" => 60);
+	return array("name" => $lang->rpg_stuff, "permissions" => $admin_permissions, "disporder" => 60);
 }
