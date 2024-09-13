2fbd60df485fef406bfc1294883c4044fffe4e99
Lara <75126217+little-evil-genius@users.noreply.github.com>
2024-09-13 16:54:13 +0200

    Create rpgstuff_module_meta.lang.php

---
+++ languages/deutsch_du/admin/rpgstuff_module_meta.lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$l['rpg_stuff'] = "RPG Erweiterungen";
+
+$l['stylesheet_updates'] = "Stylesheets überprüfen";
+$l['plugin_updates'] = "Plugins aktualisieren";
+$l['sidebar'] = "Updates";
+
+$l['can_updates_stylesheet'] = "Kann Stylesheets von Plugins zum Master Style hinzufügen?";
+$l['can_updates_plugin'] = "Kann Updates von Plugins durchführen?";
