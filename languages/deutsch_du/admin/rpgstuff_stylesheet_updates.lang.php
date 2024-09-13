702b290b8dbba53073bc7d3bb55984d9ef25eb48
Lara <75126217+little-evil-genius@users.noreply.github.com>
2024-09-13 16:55:01 +0200

    Create rpgstuff_stylesheet_updates.lang.php

---
+++ languages/deutsch_du/admin/rpgstuff_stylesheet_updates.lang.php
@@ -0,0 +1,8 @@
+<?php
+$l['stylesheets'] = "Stylesheets überprüfen";
+$l['stylesheets_desc'] = "Nach einem MyBB Upgrade fehlen die Stylesheets? Hier kannst du den Standard Stylesheet erneut neu hinzufügen.";
+$l['stylesheets_table'] = "Finde fehlende Stylesheets";
+
+$l['stylesheets_masterstyle'] = "im MyBB Master Style vorhanden";
+$l['stylesheets_add'] = "zum MyBB Master Style hinzufügen";
+$l['stylesheets_flash'] = "Der Stylesheet wurde erfolgreich zum MyBB Master Style hinzugefügt.";
