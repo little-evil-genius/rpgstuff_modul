e28b4878ba07812e3532e4da4412359ee90278b1
Lara <75126217+little-evil-genius@users.noreply.github.com>
2025-04-24 14:23:43 +0200

    Add files via upload

---
+++ inc/languages/deutsch_sie/admin/rpgstuff_plugin_updates.lang.php
@@ -0,0 +1,8 @@
+<?php
+$l['plugins'] = "Plugins aktualisieren";
+$l['plugins_desc'] = "Neue Version von einem RPG-Plugin hochgeladen und es muss ein Update erfolgen? Das kann hier vorgenommen werden.";
+$l['plugins_table'] = "Finde zu aktualisierende Plugins";
+
+$l['plugins_actual'] = "auf der aktuellsten Version";
+$l['plugins_update'] = "Update ausführen";
+$l['plugins_flash'] = "Das Update wurde erfolgreich durchgeführt.";
