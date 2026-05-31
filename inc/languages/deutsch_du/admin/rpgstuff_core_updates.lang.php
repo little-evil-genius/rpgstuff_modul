4766dd09b39d5b1910431f008d22f283a12fab03
Lara <75126217+little-evil-genius@users.noreply.github.com>
2026-05-31 09:03:37 +0200

    Add files via upload

---
+++ inc/languages/deutsch_du/admin/rpgstuff_core_updates.lang.php
@@ -0,0 +1,8 @@
+<?php
+$l['core'] = "Core-Änderungen erneut hinzufügen";
+$l['core_desc'] = "Bei einem MyBB Update können Core-Dateien verändert werden, welche von Plugins verändert wurden. Hier können die Änderungen wieder hinzugefügt werden.";
+$l['core_table'] = "Finde zu aktualisierende Core-Änderungen";
+
+$l['core_actual'] = "Core-Änderungen vorhanden";
+$l['core_update'] = "Core-Änderungen hinzufügen";
+$l['core_flash'] = "Die Core-Datei wurde erfolgreich angepasst.";
