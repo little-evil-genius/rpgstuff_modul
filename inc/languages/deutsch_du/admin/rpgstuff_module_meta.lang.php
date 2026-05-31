ad4723374e0dd2efeffd4ba50b0065ce91920c58
Lara <75126217+little-evil-genius@users.noreply.github.com>
2026-05-31 09:03:08 +0200

    Update rpgstuff_module_meta.lang.php

--- inc/languages/deutsch_du/admin/rpgstuff_module_meta.lang.php
+++ inc/languages/deutsch_du/admin/rpgstuff_module_meta.lang.php
@@ -4,7 +4,9 @@ $l['rpg_stuff'] = "RPG Erweiterungen";
 
 $l['stylesheet_updates'] = "Stylesheets überprüfen";
 $l['plugin_updates'] = "Plugins aktualisieren";
+$l['core_updates'] = "Core-Änderungen aktualisieren";
 $l['sidebar'] = "Updates";
 
 $l['can_updates_stylesheet'] = "Kann Stylesheets von Plugins zum Master Style hinzufügen?";
 $l['can_updates_plugin'] = "Kann Updates von Plugins durchführen?";
+$l['can_updates_core'] = "Kann Core-Änderungen nach MyBB Updates erneut hinzufügen?";
